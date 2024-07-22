9fad483b47a2ece5e22085ec1e197f81613ff3ec
dexterski007 <mbouj007@gmail.com>
2024-07-22 18:29:45 +0100

    oneline

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,2 +1,3 @@
-const concatArrays = (array1, array2, string) => [ ...array1, ...array2, ...string ];
-export default concatArrays;
+export default function concatArrays (array1, array2, string) {
+ return [ ...array1, ...array2, ...string ];
+}
