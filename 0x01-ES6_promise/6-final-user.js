c0c07f5e967da301d1b2a2bc06803e1a25dd6b9b
dexterski007 <mbouj007@gmail.com>
2024-07-24 00:05:10 +0100

    allsettled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup([firstName, lastName, fileName]) {
+  const promiseUser = signUpUser([firstName, lastName]);
+  const promisePhoto = uploadPhoto(fileName);
+
+  return Promise.allSettled([promiseUser, promisePhoto])
+    .then((results) => {
+      results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
+    });
+}
