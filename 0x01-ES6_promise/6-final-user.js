d5e7172c0f685852dbb25e345739ac274f2e2411
dexterski007 <mbouj007@gmail.com>
2024-07-24 00:15:32 +0100

    help

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup([firstName, lastName, fileName]) {
-  const promiseUser = signUpUser([firstName, lastName]);
-  const promisePhoto = uploadPhoto(fileName);
-
   return Promise
-    .allSettled([promiseUser, promisePhoto])
+    .allSettled([signUpUser([firstName, lastName]), uploadPhoto(fileName)])
     .then((results) => {
       results.map(({ status, value, reason }) => ({
         status,
