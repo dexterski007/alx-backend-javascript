3fa73bd1cafb2b2d1878004754d4021dcccfa793
dexterski007 <mbouj007@gmail.com>
2024-07-23 19:38:05 +0100

    moumou

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return ({ status: 200, body: 'success' });
+    })
+    .catch((Error) => {
+      console.error(Error);
+    });
 }
