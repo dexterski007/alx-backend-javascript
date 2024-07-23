6acaec752b598f888663b372a03c2cc04cb6e375
dexterski007 <mbouj007@gmail.com>
2024-07-23 23:13:56 +0100

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promisePhoto = uploadPhoto();
+  const promiseUser = createUser();
+  Promise.all([promisePhoto, promiseUser]).then((values) => {
+    console.log(values[0].body, values[1].firstName, values[1].lastName);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
