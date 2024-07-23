1a799a1dbdef8777c6dec9a0caeed2cb7c7527c7
dexterski007 <mbouj007@gmail.com>
2024-07-23 23:16:04 +0100

    fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const promisePhoto = uploadPhoto();
   const promiseUser = createUser();
   Promise.all([promisePhoto, promiseUser]).then((values) => {
-    console.log(values[0].body, values[1].firstName, values[1].lastName);
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
   });
