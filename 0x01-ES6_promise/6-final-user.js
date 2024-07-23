b869d8b5b444413e4312f62dad0b3220a67ad231
dexterski007 <mbouj007@gmail.com>
2024-07-24 00:12:38 +0100

    pending

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,12 @@ export default function handleProfileSignup([firstName, lastName, fileName]) {
   const promiseUser = signUpUser([firstName, lastName]);
   const promisePhoto = uploadPhoto(fileName);
 
-  return Promise.allSettled([promiseUser, promisePhoto])
+  return Promise
+    .allSettled([promiseUser, promisePhoto])
     .then((results) => {
-      results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
+      results.map(({ status, value, reason }) => ({
+        status,
+        value: status === 'fulfilled' ? value : reason.toString(),
       }));
     });
 }
