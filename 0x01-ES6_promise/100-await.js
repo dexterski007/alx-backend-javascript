3cddaa59b48c8ef8c6739f2d1c496f5f7c082677
dexterski007 <mbouj007@gmail.com>
2024-07-24 13:02:36 +0100

    walid

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const [uploadPromise, userPromise] = await Promise.all([uploadPhoto(), createUser()]);
+  const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
   try {
-    return { photo: uploadPromise, user: userPromise };
+    return { photo, user };
   } catch (error) {
     return { photo: null, user: null };
   }
