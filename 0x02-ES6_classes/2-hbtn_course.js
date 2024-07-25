d485e3fe35ef0b79a8dd584c9573caeec29557e0
dexterski007 <mbouj007@gmail.com>
2024-07-25 13:25:16 +0100

    holberton task

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length === 'number' && !isNaN(length)) {
+      this._length = length;
+    } else {
+      throw TypeError('Name must be a number');
+    }
+    if (Array.isArray(students) && students.every(item => typeof item === 'string')) {
+      this._students = students;
+    } else {
+      throw TypeError('Name must be an array of strings');
+    }
+  }
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+  get length() {
+    return this._length;
+  }
+  set length(length) {
+    if (typeof length === 'number' && !isNaN(length)) {
+      this._length = length;
+    } else {
+      throw TypeError('Name must be a number');
+    }
+  }
+  get students() {
+    return this._students;
+  }
+  set students(students) {
+    if (Array.isArray(students) && students.every(item => typeof item === 'string')) {
+      this._students = students;
+    } else {
+      throw TypeError('Name must be an array of strings');
+    }
+  }
+}
