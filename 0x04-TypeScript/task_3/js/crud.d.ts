62dd060db02c06ed1a9fc3951faf14391b0529dc
dexterski007 <mbouj007@gmail.com>
2024-07-31 21:37:36 +0100

    task 3

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,1 +1,5 @@
 import {RowID, rowElement} from './interface';
+
+export function insertRow(row: RowElement): number;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, rowElement:RowElement): number;
