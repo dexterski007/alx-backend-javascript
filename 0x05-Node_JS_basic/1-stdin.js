240ebdd39f4f91a6190d336919c652f4de472fb7
dexterski007 <mbouj007@gmail.com>
2024-09-13 23:02:37 +0100

    nodejs

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,6 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  const message = data.toString();
+  process.stdout.write(`Your name is: ${message}`);
+  process.stdout.write('This important software is now closing\n');
+});
