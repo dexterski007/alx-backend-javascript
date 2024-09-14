55b4c111b183605b1b2a7339445171a05129a255
dexterski007 <mbouj007@gmail.com>
2024-09-14 20:25:33 +0100

    sigterm

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  const message = data.toString();
-  process.stdout.write(`Your name is: ${message}`);
+  if (data !== null) {
+    const message = data.toString();
+    process.stdout.write(`Your name is: ${message}`);
+  }
 });
-process.on('end', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
