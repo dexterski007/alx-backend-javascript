ef2ec65c790a0bd0253d2fa4c162c6a3e9864074
dexterski007 <mbouj007@gmail.com>
2024-09-14 20:20:13 +0100

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,9 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const message = data.toString();
   process.stdout.write(`Your name is: ${message}`);
+});
+process.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-process.on('SIGTERM', function() {
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-})
