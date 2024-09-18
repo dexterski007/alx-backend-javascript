f7cdbdeab5f0a8a58fae9b4fa3670abc929855e5
dexterski007 <mbouj007@gmail.com>
2024-09-18 23:39:20 +0100

    fix

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,12 +1,30 @@
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
 
 describe('sendPaymentRequestToApi', function() {
-  it('should call utils calculatenumber', function() {
-    const spy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(90, 30);
-    sinon.assert(calledWith(spy, 'SUM', 90, 30));
+  let spy;
+
+  beforeEach(function() {
+    spy = sinon.spy(Utils, 'calculateNumber');
+  });
+
+  afterEach(function() {
     spy.restore();
+  });
+
+  it('should call utils calculatenumber', function() {
+    sendPaymentRequestToApi(90, 30);
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 90, 30)).to.be.true;
+  });
+
+  it('should log the correct total', function() {
+    const consoleSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(90, 30);
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    consoleSpy.restore();
   });
 });
