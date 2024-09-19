160a82ca032730828cf733afc5ff90688d018828
dexterski007 <mbouj007@gmail.com>
2024-09-19 01:11:02 +0100

    task 9

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,45 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('is index ok ?', () => {
+  const apiUrl = 'http://localhost:7865';
+
+  it('correct status', (done) => {
+    request.get(apiUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('correct content', (done) => {
+    request.get(apiUrl, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('is cart ok ?', () => {
+  const apiUrl = 'http://localhost:7865';
+
+  it('correct status', (done) => {
+    request.get(`${apiUrl}/cart/21`, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('correct content', (done) => {
+    request.get(`${apiUrl}/cart/21`, (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 21');
+      done();
+    });
+  });
+
+  it('correct content', (done) => {
+    request.get(`${apiUrl}/cart/str`, (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
